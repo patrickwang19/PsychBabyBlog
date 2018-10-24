43641e51e5b4797b6ae4631f03c478c642e5794e
Patrick Wang <patrick.bohan.wang@gmail.com>
2018-10-24 16:10:45 +0000

    back always visible

--- logs/script.js
+++ logs/script.js
@@ -12,3 +12,7 @@ var ajax = (filename) => {
 }
 console.log(ajax('content'));
 $('.content article').html(`<p>${ajax('content').replace(/\\n/g, '</p><p>').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/&/g, '&amp;')}</p>`);
+var backpos = $('.content .back').css('top');
+$(window).scroll(() => {
+    $('.content .back').css('top', `${Number(backpos.substring(0, backpos.length - 2)) + $(window).scrollTop()}px`)
+});
